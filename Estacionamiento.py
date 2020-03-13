2a4cf0fcf76c650d4b9fa317a410b09520a7c7bf
Karol Castellanos <karolcastellanos99@gmail.com>
2020-03-13 17:42:08 -0600

    Funciones de agregar un vehiculo al estacionamiento

--- Estacionamiento.py
+++ Estacionamiento.py
@@ -26,6 +26,10 @@ class VehiculoCliente():
         else:
             print("Plataforma no valida")
     
+    def enter(self):
+        """Hace una pausa mientras espera que se precione una tecla"""
+        input("\nPresione una tecla para continuar")
+    
     def Menu(self):
         """
         Se encarga de mostrar las opciones validas del menu
@@ -39,10 +43,41 @@ class VehiculoCliente():
                  -- 5. Salir                             --
                  ------------------------------------------
                  """)
-            
+    def __init__(self):
+        """Procesa la opcion elegida por el usuario """
+        self.vehiculo = list()
+        self.marca = list()
+        self.modelo = list()
+        self.tipo_vehiculo = list()
+        self.opcion = {"1": self.ingreso_vehiculo(),
+                     "2": self.salida_vehiculo(),
+                     "3": self.buscar(),
+                     "4": self.reporte_diario(),
+                     "5": self.salir()}
+        
+    def ingreso_vehiculo(self):
+        """Agrega un nuevo vehiculo al momento de ingresar al estacionamiento"""
+        print("------------------Ingreso de vehiculo------------------")
+        self.vehiculo.append(input("\nNumero de Placa: "))
+        self.marca.append(input("\nMarca: "))
+        self.modelo.append(input("\nModelo: "))
+        self.tipo_vehiculo.append(input("\nTipo de vehiculo: "))
+        self.enter()
+    
+    def salida_vehiculo(self):
+        """Ingresa la salida de un vehiculo y muestra los datos correspondientes del ticket"""
+
+
+    def buscar(self):
+        """Busca un vehiculo por el numero de placa"""
         
 
+    def reporte_diario(self):
+        """Muestra el reporte diario de los vehiculos que ingresaron al estacionamiento"""
 
-        
 
+    def salir(self):
+        """Finaliza el programa"""
+        print("Gracias por utilizar nuestro sistema")
+        sys.exit
     
