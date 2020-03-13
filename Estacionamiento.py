96f61aa0674dfe40de744dcadaaff7eb5fcaa577
Karol Castellanos <karolcastellanos99@gmail.com>
2020-03-13 16:33:42 -0600

    Creacion del archivo para realizar el programa

---
+++ Estacionamiento.py
@@ -0,0 +1,5 @@
+# -*- Coding utf8 -*-
+#Programa: Estacionamiento.py
+#Objetivo: Crear un servicio de estacionamiento de Vehiculos para clientes Particulares
+#Autor: Karol Castellanos
+#Fecha: 13 de Marzo 2020
